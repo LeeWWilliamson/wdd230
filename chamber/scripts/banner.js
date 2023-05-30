58bd07a60310d818178e9b8a3603ea5d5f349522
LeeWWilliamson <leewwilliamson@gmail.com>
2023-05-29 18:19:08 -0700

    Added banner and JS

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,11 @@
+
+let d = new Date().getDay();
+
+const banner = document.getElementById("banner")        
+  if (d === 1 || d === 2) {
+      banner.style.display = "block";
+    }
+    else {
+      banner.style.display = "none";
+    }
+
