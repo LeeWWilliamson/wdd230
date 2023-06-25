4709befc153fecad500e5505c723861cc5405824
LeeWWilliamson <104240469+LeeWWilliamson@users.noreply.github.com>
2023-06-24 22:45:53 -0700

    Adding Weather API to Main page

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,13 +1,57 @@
-const temp1 = document.querySelector("#temperature");
-const wind1 = document.querySelector("#wind-speed");
-let temp = parseInt(temp1.textContent);
-let wind = parseInt(wind1.textContent);
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector('#wind-speed');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Bothell&units=imperial&appid=c3b80b0b52a3df6207da3081ae49e184';
 
-if(temp <= 50 && wind > 3){
-    let chill = 35.74 + (0.6215 * temp) - (35.75 * (wind ** 0.16)) + (0.4275 * temp * (wind ** 0.16));
-    chill = Math.round(chill);
-    document.querySelector("#wind-chill").textContent = chill + "℉";
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+  }
+
 }
-else{
-    document.querySelector("#wind-chill").textContent = temp1 + "℉"; // = "N/A";
+
+function  displayResults(weatherData) {
+  const temp = weatherData.main.temp.toFixed(0)
+  currentTemp.innerHTML = `<strong>${temp}</strong>&deg;F`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+
+  const speed = weatherData.wind.speed.toFixed(0)
+  windSpeed.innerHTML = `${speed} mph`;
+  calc_windChill(temp, speed);
+
 }
+
+function calc_windChill(temp, speed){
+  if (temp <= 50  && speed >= 3.0) {
+    const chill = 
+        35.74 + 
+        0.6215 * temp - 
+        35.75 * speed ** 0.16 +
+        0.4275 * temp * speed ** 0.16;
+
+    document.getElementById("wind-chill").textContent = Math.round(chill);
+  }
+  else {
+      document.getElementById("wind-chill").textContent = "N/A";
+    // document.querySelector("#wind-chill").textContent = temp + "℉";
+
+  }
+}
+
+apiFetch();
+
